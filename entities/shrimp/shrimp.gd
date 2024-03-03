ec33b55fc6b34525bf7b93ab18325789b06345cc
yopox <yopoxdev@gmail.com>
2024-03-03 23:32:25 +0100

    [entities] add shrimps and bubbles

---
+++ entities/shrimp/shrimp.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@onready var stats: Stats = $Stats
+
+var attacking = false
+var hero: Hero = null
+var bubble = preload("res://attacks/bubble.tscn")
+
+func _process(delta):
+	if attacking and stats.shoot():
+		shoot()
+
+
+func shoot():
+	var bullet: RigidBody2D = bubble.instantiate()
+	var attack_dir = hero.position - position
+	bullet.position.x = position.x + 10 * cos(attack_dir.angle())
+	bullet.position.y = position.y + 10 * sin(attack_dir.angle())
+	bullet.apply_impulse(attack_dir)
+	
+	get_parent().add_child(bullet)
+
+
+func _on_zone_body_entered(body):
+	if not body is Hero:
+		return
+	attacking = true
+	hero = body
+
+
+func _on_zone_body_exited(body):
+	if not body is Hero:
+		return
+	attacking = false
