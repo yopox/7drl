99e76b056acfd081849206a833f334061f62f414
yopox <yopoxdev@gmail.com>
2024-03-04 22:27:49 +0100

    [hero] fix arrows spawn

--- entities/hero/hero.gd
+++ entities/hero/hero.gd
@@ -30,8 +30,8 @@ func attack():
 	
 	var bullet: RigidBody2D = arrow.instantiate()
 	bullet.stats = stats
-	bullet.position.x = position.x + 10 * cos(attack_dir.angle())
-	bullet.position.y = position.y + 10 * sin(attack_dir.angle())
+	bullet.position.x = position.x + 6 * cos(attack_dir.angle())
+	bullet.position.y = position.y + 6 * sin(attack_dir.angle())
 	bullet.rotation = attack_dir.angle() - PI / 2.0
 	bullet.apply_impulse(attack_dir)
 	
