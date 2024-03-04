d76aedb85fdca97e7424acbd30e68f6a2a19982a
ThronoCrigger <160931471+ThronoCrigger@users.noreply.github.com>
2024-03-04 01:01:35 +0100

    [sound] loop regions while entering battle

--- entities/hero/FightZone.gd
+++ entities/hero/FightZone.gd
@@ -1,10 +1,18 @@
 extends Area2D
-
+@export var event: EventAsset
+var instance: EventInstance
 
 func _ready():
-	pass
+	instance = FMODRuntime.create_instance(event)
+	instance.start()
+	instance.set_parameter_by_name_with_label("EnterCombatTest", "InBattle", false)
 
 
 func _process(delta):
 	var enemies = get_overlapping_bodies()
 	var enemy_count = enemies.size()
+	if enemy_count >= 1: 
+		instance.set_parameter_by_name_with_label("EnterCombatTest", "InBattle", true)
+	else:
+		instance.set_parameter_by_name_with_label("EnterCombatTest", "InBattle", false)
+		instance.set_parameter_by_name_with_label("EnterCombatTest", "OutBattle", true)
