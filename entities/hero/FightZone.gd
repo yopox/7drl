cdf360dd587338519df36a16eb1efe4e5161a82a
ThronoCrigger <160931471+ThronoCrigger@users.noreply.github.com>
2024-03-06 22:26:42 +0100

    [sound] elite music loop impl

--- entities/hero/FightZone.gd
+++ entities/hero/FightZone.gd
@@ -12,11 +12,16 @@ func update_instance():
 	var enemies = get_overlapping_bodies()
 	var has_elites = enemies.filter(func(e): return e is Enemy and (e as Enemy).stats.elite).size() > 0
 	var enemy_count = enemies.size()
-	if enemy_count >= 1: 
+	if enemy_count >= 1 and has_elites == false: 
 		instance.set_parameter_by_name_with_label("EnterCombatTest", "InBattle", true)
 	else:
 		instance.set_parameter_by_name_with_label("EnterCombatTest", "InBattle", false)
 		instance.set_parameter_by_name_with_label("EnterCombatTest", "OutBattle", true)
+	if has_elites == true: 
+		instance.set_parameter_by_name_with_label("EnterCombatTest", "OutBattle", false)
+		instance.set_parameter_by_name_with_label("EnterCombatTest", "InBattle", false)
+		instance.set_parameter_by_name_with_label("EnterCombatTest", "InBattleElite", true)
+
 
 
 func _on_body_entered(_body):
