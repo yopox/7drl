d17208f58eb8b4c2999418ad8c5d67baf08fd8cb
ThronoCrigger <160931471+ThronoCrigger@users.noreply.github.com>
2024-03-08 22:48:40 +0100

    [sound] wizard + grabitem impl

--- attacks/wiz_zone.gd
+++ attacks/wiz_zone.gd
@@ -2,7 +2,12 @@ class_name WizZone extends RigidBody2D
 
 @onready var zone: Area2D = $Area2D
 var stats: Stats
+@export var cast_event: EventAsset
+var cast_sfx: EventInstance
 
+func _ready():
+	cast_sfx = FMODRuntime.create_instance(cast_event)
+	cast_sfx.start()
 
 func collide():
 	for body in zone.get_overlapping_bodies():
