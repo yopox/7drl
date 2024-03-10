3f3bf29b7264604774770351422c23ccdf55faef
ThronoCrigger <160931471+ThronoCrigger@users.noreply.github.com>
2024-03-10 02:30:39 +0100

    [gd+sound] boss music impl + cave weights impl

--- scenes/BGM.gd
+++ scenes/BGM.gd
@@ -3,17 +3,19 @@ class_name BGM extends Node
 @export var event_lvl1: EventAsset
 @export var event_lvl2: EventAsset
 @export var event_lvl3: EventAsset
+@export var event_boss: EventAsset
 
 var instance_lvl1: EventInstance
 var instance_lvl2: EventInstance
 var instance_lvl3: EventInstance
-
+var instance_boss: EventInstance
 
 func _ready():
 	Util.BGM = self
 	instance_lvl1 = FMODRuntime.create_instance(event_lvl1)
 	instance_lvl2 = FMODRuntime.create_instance(event_lvl2)
 	instance_lvl3 = FMODRuntime.create_instance(event_lvl3)
+	instance_boss = FMODRuntime.create_instance(event_boss)
 
 
 func set_parameter(name: String, label: String, ignore_seek_speed: bool):
@@ -36,6 +38,12 @@ func play_lvl2():
 func play_lvl3():
 	instance_lvl3.start()
 	for i in [instance_lvl1, instance_lvl2]:
+		i.stop(FMODStudioModule.FMOD_STUDIO_STOP_ALLOWFADEOUT)
+
+
+func play_boss():
+	instance_boss.start()
+	for i in [instance_lvl1, instance_lvl2, instance_lvl3]:
 		i.stop(FMODStudioModule.FMOD_STUDIO_STOP_ALLOWFADEOUT)
 
 
